b14ae48b27ac39e03a0d65c5e15a0411ae88eae5
wimbox <winbox15@gmail.com>
2026-02-06 17:54:50 +0200

    Activate Cloud Sync

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -3,12 +3,14 @@
  * Replace the placeholders below with your project credentials from Firebase Console
  */
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCDXpDM1Q--_1FQb0KumpyiBU6_5oTKcuI",
+    authDomain: "prescription-app-70fc7.firebaseapp.com",
+    databaseURL: "https://prescription-app-70fc7-default-rtdb.firebaseio.com",
+    projectId: "prescription-app-70fc7",
+    storageBucket: "prescription-app-70fc7.appspot.com",
+    messagingSenderId: "1052652776484",
+    appId: "1:1052652776484:web:03d2ddbc1f08351b3afcb1",
+    measurementId: "G-QGK4Z3MEKN"
 };
 
 // Initialize Firebase (Compat mode for easier integration)
